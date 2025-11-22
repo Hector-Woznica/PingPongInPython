6f62545d9beaecd89c045b820989bde94eae431f
umair <eeumairali@gmail.com>
2025-11-22 13:52:40 +0500

    updated rod cotinous movement

--- main.py
+++ main.py
@@ -6,21 +6,19 @@ from turtle import Turtle, Screen
 
 def move_left_rod_up():
     if Rod_Left.ycor()<450:
-        Rod_Left.forward(20)
+        rod_positions[0] = True
+        
 
 def move_left_rod_down():
     if Rod_Left.ycor()>-450:
-        Rod_Left.forward(-20)
+        rod_positions[1] = True
 
 def move_right_rod_up():
     if Rod_Right.ycor()<450:
-        Rod_Right.forward(20)
-
+        rod_positions[2] = True
 def move_right_rod_down():
     if Rod_Right.ycor()>-450:
-        Rod_Right.forward(-20)
-
-
+        rod_positions[3] = True
 
 screen = Screen()
 screen.setup(width=1.0,height=1.0)
@@ -63,11 +61,27 @@ turtle.onkeypress(move_left_rod_down, "s")
 turtle.onkeypress(move_right_rod_up, "Up")
 turtle.onkeypress(move_right_rod_down, "Down")
 
-# hector
+turtle.onkeyrelease(lambda: rod_positions.__setitem__(0, False), "w")  # hector
+turtle.onkeyrelease(lambda: rod_positions.__setitem__(1, False), "s")  # hector
+turtle.onkeyrelease(lambda: rod_positions.__setitem__(2, False), "Up")  # hector
+turtle.onkeyrelease(lambda: rod_positions.__setitem__(3, False), "Down")  # hector
 
-    
+rod_positions = [False, False, False, False]  # [left_up, left_down, right_up, right_down]
+# keep checking positioons 
 
 
+# keep checking about 100 millisec rod positions
+def check_rod_positions():
+    if rod_positions[0]:
+        Rod_Left.sety(Rod_Left.ycor() + 20)
+    if rod_positions[1]:
+        Rod_Left.sety(Rod_Left.ycor() - 20)
+    if rod_positions[2]:
+        Rod_Right.sety(Rod_Right.ycor() + 20)
+    if rod_positions[3]:
+        Rod_Right.sety(Rod_Right.ycor() - 20)
+    turtle.ontimer(check_rod_positions, 100)
+check_rod_positions()
 
 
 
